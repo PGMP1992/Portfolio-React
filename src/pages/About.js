9eea1937d8c84d733e8833daa453de9a13c78c84
PGMP1992 <pedro@dkltd.net>
2024-06-20 11:17:36 +0200

    Changed About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,34 +8,22 @@ export default function About() {
               A tenetur dolor quod iusto consectetur! Voluptate necessitatibus sit iure accusamus error.
           </p>
       
-      <div class="row">
-          <h3>Skills</h3>
+        <div class="row">
+            <h3>Skills</h3>
+          
+            <p>Front End - HTML, CSS and Javascript</p>
+            
+            <p>C#, Pascal and C++ </p>
+            
+            <p>React</p>
+            
+            <p>Back End SQL - MS Sql, Oracle and MySQL </p>
+            
+        </div>
         
-          <p>Front End - HTML, CSS and Javascript</p>
-          <div class="container">
-            <div class="front">30%</div>
-          </div>
-
-          <p>C#, Pascal and C++ </p>
-          <div class="container">
-            <div class="c">80%</div>
-          </div>
-
-          <p>React</p>
-          <div class="container">
-            <div class="react">10%</div>
-          </div>
-
-          <p>Back End SQL - MS Sql, Oracle and MySQL </p>
-          <div class="container">
-            <div class="sql">80%</div>
-          </div>
+        <div id="cv">
+          <a href="images\pedro.docx" download>Download Resume</a>
+        </div>
       </div>
-      
-      <div id="cv">
-        <a href="images\pedro.docx" download>Download Resume</a>
-      </div>
-          </div>
     )
-  }
-  
+  }  
